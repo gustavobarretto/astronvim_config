d11b09f42b652c527d069e93ce0aaee98436de21
gustavo barretto <gustavobarretto@gmail.com>
2024-05-08 19:17:57 -0300

    changes

--- plugins/user.lua
+++ plugins/user.lua
@@ -23,25 +23,36 @@ return {
       },
     }) end,
   },
-  { "EdenEast/nightfox.nvim" }, -- lazy
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require 'nordic' .load()
+    end
+  },
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
+    config = function()
+      vim.keymap.set('i', '<C-]>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<C-[>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
   },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { -- further customize the options set by the community
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  }
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- { -- further customize the options set by the community
+  --   "zbirenbaum/copilot.lua",
+  --   opts = {
+  --     suggestion = {
+  --       keymap = {
+  --         accept = "<C-l>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<C-.>",
+  --         prev = "<C-,>",
+  --         dismiss = "<C/>",
+  --       },
+  --     },
+  --   },
+  -- }
 }
